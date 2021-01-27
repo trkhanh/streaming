7f5bbd6251e1b26dee21a3ebc7f778f8918bb68d
Tran Khanh <tran_khanh@outlook.com>
2021-01-27 23:49:58 +0700

    implement backend stuff

---
+++ backend/user.js
@@ -0,0 +1,25 @@
+const users = {
+    'trkhanh8@gmail.com': {
+        info: {
+            id: '1a363a59-ce41-426f-8910-7edaec12d8ae',
+            email: 'trkhanh8@gmail.com',
+            firstName: 'Mattia',
+            lastName: 'Cattarinussi',
+        },
+        password: 'pass',
+    },
+    'guest@example.com': {
+        info: {
+            id: '9b31a8c5-c319-466c-a284-65fe4e9e3cd6',
+            email: 'guest@example.com',
+            firstName: 'Guest',
+            lastName: 'Foo',
+        },
+        password: 'pass',
+    },
+}
+
+const getUser = email => users[email] || null;
+module.exports = {
+    getUser,
+};
